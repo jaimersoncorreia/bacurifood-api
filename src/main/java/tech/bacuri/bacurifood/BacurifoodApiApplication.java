5f0ec29cdd1eb1d4bfa6939d58e1a3051cc9245a
Jaimerson <jaimersonsantos@ufam.edu.br>
2023-12-31 18:59:05 -0400

    2.5 Criando um projeto Spring Boot com Spring Initializr

---
+++ src/main/java/tech/bacuri/bacurifood/BacurifoodApiApplication.java
@@ -0,0 +1,13 @@
+package tech.bacuri.bacurifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacurifoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacurifoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/bacuri/bacurifood/BacurifoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bacuri.bacurifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacurifoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
