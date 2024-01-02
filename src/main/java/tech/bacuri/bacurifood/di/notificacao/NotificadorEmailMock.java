e250156ede14f8c11c92223a7edcc222568ecaa1
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-01 20:36:05 -0400

    2.20. Mudando o comportamento da aplicação com Spring Profiles

--- src/main/java/tech/bacuri/bacurifood/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/tech/bacuri/bacurifood/di/notificacao/NotificadorEmailMock.java
@@ -4,17 +4,17 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import tech.bacuri.bacurifood.di.modelo.Cliente;
 
-@Profile("prod")
+@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
-public class NotificadorEmail implements Notificador {
-    public NotificadorEmail() {
-        System.out.println("NotificadorEmail REAL");
+public class NotificadorEmailMock implements Notificador {
+    public NotificadorEmailMock() {
+        System.out.println("NotificadorEmail MOCK");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificado %s através do e-mail %s: %s\n",
+        System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
