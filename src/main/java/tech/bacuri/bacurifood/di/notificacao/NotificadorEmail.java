f4d1fef629e406610391b42de84e25b76f5dbf31
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-03 16:56:49 -0400

    2.27. Alterando a configuração do projeto dependendo do ambiente (com Spring Profiles)

--- src/main/java/tech/bacuri/bacurifood/di/notificacao/NotificadorEmail.java
+++ src/main/java/tech/bacuri/bacurifood/di/notificacao/NotificadorEmail.java
@@ -1,19 +1,21 @@
 package tech.bacuri.bacurifood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import tech.bacuri.bacurifood.di.modelo.Cliente;
 
-@Profile("prod")
+@Profile("prd")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
-    public NotificadorEmail() {
-        System.out.println("NotificadorEmail REAL");
-    }
+    @Autowired
+    private NotificadorProperties properties;
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("host = " + properties.getHostServidor());
+        System.out.println("porta = " + properties.getPortaServidor());
         System.out.printf("Notificado %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
