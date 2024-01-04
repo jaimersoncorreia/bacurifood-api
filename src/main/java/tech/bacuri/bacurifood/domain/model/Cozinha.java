19f55038ce46b80ca26b9b9fd60574106e1e29b1
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-03 21:44:26 -0400

    3.6. Mapeando o id da entidade para autoincremento

--- src/main/java/tech/bacuri/bacurifood/domain/model/Cozinha.java
+++ src/main/java/tech/bacuri/bacurifood/domain/model/Cozinha.java
@@ -4,10 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -17,6 +14,7 @@ import javax.persistence.Table;
 public class Cozinha {
     @EqualsAndHashCode.Include
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "nom_cozinha")
