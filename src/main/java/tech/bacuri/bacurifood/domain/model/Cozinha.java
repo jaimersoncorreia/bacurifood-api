fe2b8110bbb0cab05f5576332970d55a618ce8bf
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-06 17:46:57 -0400

    4.15. Customizando as representações XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName

--- src/main/java/tech/bacuri/bacurifood/domain/model/Cozinha.java
+++ src/main/java/tech/bacuri/bacurifood/domain/model/Cozinha.java
@@ -1,9 +1,12 @@
 package tech.bacuri.bacurifood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.*;
 
 import javax.persistence.*;
 
+@JsonRootName("gastronomia")
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -18,6 +21,8 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+//    @JsonIgnore
+    @JsonProperty(value = "titulo")
     @Column(nullable = false)
     private String nome;
 }
