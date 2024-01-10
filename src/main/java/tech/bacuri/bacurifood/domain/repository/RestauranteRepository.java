665b8da9e865dc9e9a2a17d06e3c6e18073fc43f
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-09 22:37:54 -0400

    5.7. Usando as keywords para definir critérios de query methods

--- src/main/java/tech/bacuri/bacurifood/domain/repository/RestauranteRepository.java
+++ src/main/java/tech/bacuri/bacurifood/domain/repository/RestauranteRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.bacuri.bacurifood.domain.model.Restaurante;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+    List<Restaurante> findAllByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+    List<Restaurante> findAllByNomeContainingAndCozinhaId(String nome, Long cozinha);
 }
