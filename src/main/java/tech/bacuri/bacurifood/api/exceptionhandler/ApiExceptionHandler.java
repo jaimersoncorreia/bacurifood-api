ae6e10bed169bfa8d7a0efc8023c481cd6e46ce3
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-17 17:53:28 -0400

    8.14. Desafio: implementando exception handler

--- src/main/java/tech/bacuri/bacurifood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/tech/bacuri/bacurifood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,13 +3,13 @@ package tech.bacuri.bacurifood.api.exceptionhandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import tech.bacuri.bacurifood.domain.exception.EntidadeEmUsoException;
 import tech.bacuri.bacurifood.domain.exception.EntidadeNaoEncontradaException;
 import tech.bacuri.bacurifood.domain.exception.NegocioException;
 
 import java.time.LocalDateTime;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
@@ -29,5 +29,14 @@ public class ApiExceptionHandler {
                 .mensagem(e.getMessage())
                 .build();
         return ResponseEntity.status(BAD_REQUEST).body(problema);
+    }
+
+    @ExceptionHandler(EntidadeEmUsoException.class)
+    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+        return ResponseEntity.status(CONFLICT).body(problema);
     }
 }
