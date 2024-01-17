a3cb4178072d0950924bd41f844516535aa4e026
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-17 18:10:34 -0400

    8.15. Criando um exception handler global com ResponseEntityExceptionHandler

--- src/main/java/tech/bacuri/bacurifood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/tech/bacuri/bacurifood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package tech.bacuri.bacurifood.api.exceptionhandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import tech.bacuri.bacurifood.domain.exception.EntidadeEmUsoException;
 import tech.bacuri.bacurifood.domain.exception.EntidadeNaoEncontradaException;
 import tech.bacuri.bacurifood.domain.exception.NegocioException;
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
         Problema problema = Problema.builder()
