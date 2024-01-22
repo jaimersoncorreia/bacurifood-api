79470e0b52273c70785454af7bd01ee084e1eeb2
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-22 18:47:26 -0400

    10.2. Preparando o projeto para testes de integração

--- src/test/java/tech/bacuri/bacurifood/BacurifoodApiApplicationTests.java
+++ src/test/java/tech/bacuri/bacurifood/BacurifoodApiApplicationTests.java
@@ -2,12 +2,16 @@ package tech.bacuri.bacurifood;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 class BacurifoodApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertFalse(true);
+    }
 
 }
