2600771b8a714b0464532919b0d9617f2a7b182d
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-22 19:43:42 -0400

    10.3. Criando e rodando um teste de integração com Spring Boot, JUnit e AssertJ

--- src/test/java/tech/bacuri/bacurifood/BacurifoodApiApplicationTests.java
+++ src/test/java/tech/bacuri/bacurifood/BacurifoodApiApplicationTests.java
@@ -1,17 +1,20 @@
 package tech.bacuri.bacurifood;
 
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import tech.bacuri.bacurifood.domain.service.CadastroCozinhaService;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+@AllArgsConstructor
 @SpringBootTest
 class BacurifoodApiApplicationTests {
+    private final CadastroCozinhaService cozinhaService;
 
     @Test
     void contextLoads() {
-        assertFalse(true);
+        assertFalse(false);
     }
 
 }
---
+++ src/test/java/tech/bacuri/bacurifood/domain/service/CadastroCozinhaServiceTest.java
@@ -0,0 +1,52 @@
+package tech.bacuri.bacurifood.domain.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tech.bacuri.bacurifood.domain.model.Cozinha;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class CadastroCozinhaServiceTest {
+    @Autowired
+    private CadastroCozinhaService cozinhaService;
+
+    @Test
+    void deveCadastrarCozinhaComSucesso() {
+        //cenário
+        Cozinha novaCozinha = Cozinha.builder()
+                .nome("Chinesa")
+                .build();
+
+        //ação
+        novaCozinha = cozinhaService.salvar(novaCozinha);
+
+        //validação
+        assertThat(novaCozinha).isNotNull();
+        assertThat(novaCozinha.getId()).isNotNull();
+    }
+
+    @Test
+    void deveLancarException_Quando_CadastrarCozinha_SemNome() {
+        //cenário
+        Cozinha novaCozinha = Cozinha.builder()
+                .nome(null)
+                .build();
+
+        //ação
+        ConstraintViolationException erroEsperado =
+                Assertions.assertThrows(ConstraintViolationException.class, () -> {
+                    cozinhaService.salvar(novaCozinha);
+                });
+
+        //validação
+        assertThat(erroEsperado).isNotNull();
+
+    }
+
+}
