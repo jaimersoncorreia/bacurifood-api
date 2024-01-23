b02f28b006cdbb1d80564986d78e0632acb8e25e
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-22 20:20:25 -0400

    10.6. Rodando os testes pelo Maven

--- src/test/java/tech/bacuri/bacurifood/BacurifoodApiApplicationTests.java
+++ src/test/java/tech/bacuri/bacurifood/BacurifoodApiApplicationTests.java
@@ -1,17 +1,12 @@
 package tech.bacuri.bacurifood;
 
-import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import tech.bacuri.bacurifood.domain.service.CadastroCozinhaService;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-@AllArgsConstructor
 @SpringBootTest
 class BacurifoodApiApplicationTests {
-    private final CadastroCozinhaService cozinhaService;
-
     @Test
     void contextLoads() {
         assertFalse(false);
