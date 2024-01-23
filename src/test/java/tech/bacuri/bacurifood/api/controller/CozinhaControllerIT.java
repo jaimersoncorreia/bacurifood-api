13045655f6b4e621ddfcce885a2074d2b282b49e
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-22 21:58:48 -0400

    10.8. Implementando Testes de API com REST Assured e validando o código de status HTTP

--- src/test/java/tech/bacuri/bacurifood/api/controller/CozinhaControllerIT.java
+++ src/test/java/tech/bacuri/bacurifood/api/controller/CozinhaControllerIT.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.springframework.http.HttpStatus.OK;
@@ -16,6 +17,7 @@ class CozinhaControllerIT {
 
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinha() {
+        enableLoggingOfRequestAndResponseIfValidationFails();
         given()
                 .basePath("/cozinhas")
                 .port(port)
