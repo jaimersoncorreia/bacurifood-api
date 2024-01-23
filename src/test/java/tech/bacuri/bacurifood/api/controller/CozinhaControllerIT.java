15fce17f302f45475faeadece748d0726510dddd
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-22 22:14:26 -0400

    10.9. Validando o corpo da resposta HTTP

--- src/test/java/tech/bacuri/bacurifood/api/controller/CozinhaControllerIT.java
+++ src/test/java/tech/bacuri/bacurifood/api/controller/CozinhaControllerIT.java
@@ -1,5 +1,6 @@
 package tech.bacuri.bacurifood.api.controller;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -7,6 +8,9 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,5 +30,20 @@ class CozinhaControllerIT {
                 .get()
                 .then()
                 .statusCode(OK.value());
+    }
+
+
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinha() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(JSON)
+                .when()
+                .get()
+                .then()
+                .body("", hasSize(4))
+                .body("nome", hasItems("Indiana", "Tailandesa"));
     }
 }
