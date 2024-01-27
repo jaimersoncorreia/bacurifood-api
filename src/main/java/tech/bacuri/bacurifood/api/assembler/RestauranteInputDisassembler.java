eda9eaf55470ccc666730c696b97fdec69fefaff
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-01-27 15:02:07 -0400

    11.17. Mapeando para uma instância destino (e não um tipo) com ModelMapper

--- src/main/java/tech/bacuri/bacurifood/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/tech/bacuri/bacurifood/api/assembler/RestauranteInputDisassembler.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 import tech.bacuri.bacurifood.api.model.input.RestauranteInput;
+import tech.bacuri.bacurifood.domain.model.Cozinha;
 import tech.bacuri.bacurifood.domain.model.Restaurante;
 
 
@@ -15,5 +16,12 @@ public class RestauranteInputDisassembler {
 
     public Restaurante toEntity(RestauranteInput restauranteInput) {
         return mapper.map(restauranteInput, Restaurante.class);
+    }
+
+    public void copyToEntity(RestauranteInput restauranteInput, Restaurante restaurante) {
+        /* Pra evitar org.hibernate.HibernateException: identifier of an instance of
+           tech.bacuri.bacurifood.domain.model.Cozinha was altered from 1 to 2 */
+        restaurante.setCozinha(new Cozinha());
+        mapper.map(restauranteInput, restaurante);
     }
 }
--- src/main/java/tech/bacuri/bacurifood/api/controller/RestauranteController.java
+++ src/main/java/tech/bacuri/bacurifood/api/controller/RestauranteController.java
@@ -1,11 +1,6 @@
 package tech.bacuri.bacurifood.api.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 import tech.bacuri.bacurifood.api.assembler.RestauranteInputDisassembler;
 import tech.bacuri.bacurifood.api.assembler.RestauranteModelAssembler;
@@ -16,16 +11,9 @@ import tech.bacuri.bacurifood.domain.exception.NegocioException;
 import tech.bacuri.bacurifood.domain.model.Restaurante;
 import tech.bacuri.bacurifood.domain.service.CadastroRestauranteService;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.lang.reflect.Field;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
-import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES;
-import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
-import static org.springframework.beans.BeanUtils.copyProperties;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @AllArgsConstructor
@@ -35,7 +23,7 @@ public class RestauranteController {
 
     private final CadastroRestauranteService cadastroRestauranteService;
     private final RestauranteModelAssembler assembler;
-    private final RestauranteInputDisassembler disassembler;
+    private final RestauranteInputDisassembler restauranteInputDisassembler;
 
     @GetMapping
     public List<RestauranteModel> listar() {
@@ -52,7 +40,7 @@ public class RestauranteController {
     @ResponseStatus(CREATED)
     public RestauranteModel salvar(@RequestBody @Valid RestauranteInput restauranteInput) {
         try {
-            Restaurante restaurante = disassembler.toEntity(restauranteInput);
+            Restaurante restaurante = restauranteInputDisassembler.toEntity(restauranteInput);
             return assembler.toModel(cadastroRestauranteService.salvar(restaurante));
         } catch (CozinhaNaoEncontradaException e) {
             throw new NegocioException(e.getMessage(), e);
@@ -61,38 +49,12 @@ public class RestauranteController {
 
     @PutMapping("/{restauranteId}")
     public RestauranteModel atualizar(@PathVariable Long restauranteId, @RequestBody @Valid RestauranteInput restauranteInput) {
-        Restaurante restauranteObtido;
-        restauranteObtido = cadastroRestauranteService.obter(restauranteId);
-
-        copyProperties(disassembler.toEntity(restauranteInput), restauranteObtido,
-                "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
         try {
+            Restaurante restauranteObtido = cadastroRestauranteService.obter(restauranteId);
+            restauranteInputDisassembler.copyToEntity(restauranteInput, restauranteObtido);
             return assembler.toModel(cadastroRestauranteService.atualizar(restauranteObtido));
         } catch (CozinhaNaoEncontradaException e) {
             throw new NegocioException(e.getMessage(), e);
-        }
-    }
-
-    private void merge(Map<String, Object> dadosOrigem,
-                       Restaurante restauranteDestino,
-                       HttpServletRequest request) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.configure(FAIL_ON_IGNORED_PROPERTIES, true);
-            objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, true);
-            Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
-            dadosOrigem.forEach((propriedade, valor) -> {
-                Field field = ReflectionUtils.findField(Restaurante.class, propriedade);
-                Objects.requireNonNull(field).setAccessible(true);
-
-                Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-
-                ReflectionUtils.setField(field, restauranteDestino, novoValor);
-            });
-        } catch (IllegalArgumentException e) {
-            throw new HttpMessageNotReadableException(e.getMessage(),
-                    ExceptionUtils.getRootCause(e),
-                    new ServletServerHttpRequest(request));
         }
     }
 }
